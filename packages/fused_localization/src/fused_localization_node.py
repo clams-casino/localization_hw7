f243098c006f9b77c0462d8abf4767868248c7fc
Mike <mike.zhang.619@gmail.com>
2020-11-17 01:22:01 +0100

    base fused_localization_node code

--- packages/fused_localization/src/fused_localization_node.py
+++ packages/fused_localization/src/fused_localization_node.py
@@ -11,8 +11,23 @@ import tf2_ros
 import geometry_msgs.msg
 
 
-# class PoseBuffer():
-#     self
+class EncoderATFuser():
+    
+    def __init__(self, buffer_duration):
+
+        pass
+
+    def addEncoderTF(self, tf):
+
+        pass
+
+    def atUpdate(self, tf):
+
+        pass
+
+    def publishLatestTF(self):
+
+        pass
 
 class FusedLocalizationNode(DTROS):
 
@@ -26,10 +41,9 @@ class FusedLocalizationNode(DTROS):
 
 
         # tf listeners and broadcasters
-        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10.0))   # hold the last t seconds of transforms
+        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(0.2))   # hold the last t seconds of transforms
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         self.tf_br = tf2_ros.TransformBroadcaster()
-
 
         # flag to track if apriltag service has been called yet
         self.init_at_localization = False
@@ -37,7 +51,7 @@ class FusedLocalizationNode(DTROS):
 
     def getLatestTF(self, source, target):
         try:
-            tf = self.tf_buffer.lookup_transform(source, target, rospy.Time(0))
+            tf = self.tf_buffer.lookup_transform(source, target, rospy.Time())
 
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rospy.loginfo("Could not get tf from {} to {}".format(source, target))
@@ -53,11 +67,7 @@ class FusedLocalizationNode(DTROS):
             if map_atbase is not None:
 
                 print('Got map_atbase')
-                print(map_atbase.header.stamp.to_sec())
-
-                print(map_atbase.transform.translation.x)
-                print(map_atbase.transform.translation.y)
-                print(map_atbase.transform.translation.z)
+                print(map_atbase)
 
                 if self.init_at_localization is False:
                     self.init_at_localization = True
@@ -68,24 +78,26 @@ class FusedLocalizationNode(DTROS):
 
                 # TODO what to do with this map_atbase at a certain time
 
+                # TODO Need to check if this is a new transform or we just got the last one
+
 
             map_encoderbase = self.getLatestTF('map', 'encoder_baselink')
             if map_encoderbase is not None:
                 if self.init_at_localization:   # know that the encoder tf has been initialized and we can use it
+
                     # TODO what to do with this info
                     pass
 
                 else: # Don't do anything if the encoder tf hasn't been initialized
                     pass
 
-     
-
-
             rate.sleep()
 
 
 if __name__ == '__main__':
     # Initialize the node
-    camera_node = FusedLocalizationNode(node_name='at_localization_node')
+    node = FusedLocalizationNode(node_name='fused_localization_node')
+    rospy.loginfo("fused_localization_node is up and running...")
+    node.run()
     # Keep it spinning to keep the node alive
     rospy.spin()
